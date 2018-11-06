0e977d4bda8707252280220ed3a18398a859bf50
Akashdeep Singh <akashthind007@gmail.com>
2018-11-06 08:23:39 -0500

    ADDED contact plugin

--- AKASHDEEP_C0734733_MAD4103_FP_GROUP1/platforms/android/app/src/main/assets/www/js/index.js
+++ AKASHDEEP_C0734733_MAD4103_FP_GROUP1/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,15 +1,51 @@
 var db = null;
+var userEmail = "";
+var userPassword = "";
 var storage = window.localStorage;
-
 // add event listeners
 document.addEventListener("deviceReady", connectToDatabase);
-document.getElementById("insert-hero").addEventListener("click", saveButtonPressed);
+document.addEventListener("deviceReady", saveButtonPressed);
+//document.getElementById("insert-hero").addEventListener("click", saveButtonPressed);
 document.getElementById("show-heros").addEventListener("click", showAllPressed);
 document.getElementById("rescue-me").addEventListener("click", vibration);
+document.getElementById("login-button").addEventListener("click", userLogin);
 
 function vibration() {
     navigator.vibrate(3000);
 }
+
+function userLogin()
+{
+    userEmail = document.getElementById("email").value;
+    userPassword = document.getElementById("password").value;
+    alert(userEmail + userPassword);
+
+    db.transaction(function (transaction) {
+        transaction.executeSql("SELECT * FROM users where email=? and password=?", [userEmail, userPassword],
+                function (tx, results) {
+                    var numRows = results.rows.length;
+
+                    for (var i = 0; i < numRows; i++) {
+
+                        // to get individual items:
+                        var item = results.rows.item(i);
+                        console.log(item);
+                        console.log(item.name);
+
+                        alert(item + item.name);
+
+                        // show it in the user interface
+                        document.getElementById("dbItems").innerHTML +=
+                                "<p>Name: " + item.name + "</p>"
+                                + "<p>Email : " + item.email + "</p>"
+                                + "<p>=======================</p>";
+                    }
+
+                }, function (error) {
+        });
+    });
+}
+
 function connectToDatabase() {
     console.log("device is ready - connecting to database");
     if (window.cordova.platformId === 'browser') {
@@ -31,7 +67,8 @@ function connectToDatabase() {
 }
 
 function createTables(transaction) {
-    var sql = "CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY AUTOINCREMENT, name text, isAvailable integer)";
+    var sql = "CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY AUTOINCREMENT,name text, email text, password text, age text,\n\
+ gender text, location text, phone text)";
     transaction.executeSql(sql, [], createSuccess, createFail)
 }
 
@@ -43,40 +80,54 @@ function createFail(error) {
 }
 
 
-
-
 function saveButtonPressed(transaction) {
     console.log("save!!!");
-    var str  = storage.getItem("inserted");
-    if(str != "yes")
-        {    
-    
+
     db.transaction(function (transaction) {
         // save the values to the database
-        var sql = "INSERT INTO users (name, isAvailable) VALUES ('Spiderman',1), ('Thor',1), ('Captain America',0), ('Wonder Women',0)";
+        var sql = "INSERT INTO users (name, email,password, age,gender,location,phone) \n\
+VALUES ('Akashdeep','akashthind007@gmail.com','password','20', 'Male','Toronto','+13657780293'),('Abhishek','abbansal1995@gmail.com','password', '20', 'Male','Toronto','+19057819666')";
 
-        var st  =  storage.getItem("inserted");
- 
         transaction.executeSql(sql, [], function (tx, result) {
             alert("Insert success");
+
+//============================= CREATE CONTACTS CODE=================================
+            var myContact = navigator.contacts.create({"displayName": "The New Contact"});
+            var name = new ContactName();
+            name.givenName = "Jane";
+            name.familyName = "Doe";
+            myContact.name = name;
+
+            var phoneNumbers = [];
+            phoneNumbers[0] = new ContactField('work', '212-555-1234', false);
+            phoneNumbers[1] = new ContactField('mobile', '917-555-5432', true); // preferred number
+            phoneNumbers[2] = new ContactField('home', '203-555-7890', false);
+            myContact.phoneNumbers = phoneNumbers;
+
+            myContact.note = "Example note for the newly added contact";
+
+            myContact.save(onSuccessCallBack, onErrorCallBack);
+
+            function onSuccessCallBack(contact) {
+                alert("Save Success");
+            }
+            ;
+
+            function onErrorCallBack(contactError) {
+                alert("Error = " + contactError.code);
+            }
+            ;
+
             //showAllPressed()
-
-            var storage = window.localStorage;
-            storage.setItem("inserted", "yes"); // Pass a key name and its value to add or update that key.
-
         }, function (error) {
             alert("Insert failed: " + error);
         });
     }
-        
+
     );
 
-}
 
-else
-{
-    alert("data already inserted");
-}
+
 
 }
 
@@ -95,21 +146,12 @@ function showAllPressed() {
                         var item = results.rows.item(i);
                         console.log(item);
                         console.log(item.name);
-                        var av = "";
-                        if (item.isAvailable == 1)
-                        {
-                            av = "Yes";
-                        } else
-                        {
-                            av = "No";
 
-                        }
 
                         // show it in the user interface
-                        alert(item.name);
                         document.getElementById("dbItems").innerHTML +=
                                 "<p>Name: " + item.name + "</p>"
-                                + "<p>Available  To Hire : " + av + "</p>"
+                                + "<p>Email : " + item.email + "</p>"
                                 + "<p>=======================</p>";
                     }
 
--- AKASHDEEP_C0734733_MAD4103_FP_GROUP1/platforms/browser/www/js/index.js
+++ AKASHDEEP_C0734733_MAD4103_FP_GROUP1/platforms/browser/www/js/index.js
@@ -16,9 +16,34 @@ function vibration() {
 
 function userLogin()
 {
-   userEmail = document.getElementById("email").value;
+    userEmail = document.getElementById("email").value;
     userPassword = document.getElementById("password").value;
-    alert(userEmail+userPassword);
+    alert(userEmail + userPassword);
+
+    db.transaction(function (transaction) {
+        transaction.executeSql("SELECT * FROM users where email=? and password=?", [userEmail, userPassword],
+                function (tx, results) {
+                    var numRows = results.rows.length;
+
+                    for (var i = 0; i < numRows; i++) {
+
+                        // to get individual items:
+                        var item = results.rows.item(i);
+                        console.log(item);
+                        console.log(item.name);
+
+                        alert(item + item.name);
+
+                        // show it in the user interface
+                        document.getElementById("dbItems").innerHTML +=
+                                "<p>Name: " + item.name + "</p>"
+                                + "<p>Email : " + item.email + "</p>"
+                                + "<p>=======================</p>";
+                    }
+
+                }, function (error) {
+        });
+    });
 }
 
 function connectToDatabase() {
@@ -61,10 +86,38 @@ function saveButtonPressed(transaction) {
     db.transaction(function (transaction) {
         // save the values to the database
         var sql = "INSERT INTO users (name, email,password, age,gender,location,phone) \n\
-VALUES ('Spiderman','user@example.com','password','20', 'Male','Toronto','1234567890'),('Thor','user@example.com','password', '20', 'Male','Toronto','1234567890'),('Superman','user@example.com','password','20', 'Male','Toronto','1234567890')";
+VALUES ('Akashdeep','akashthind007@gmail.com','password','20', 'Male','Toronto','+13657780293'),('Abhishek','abbansal1995@gmail.com','password', '20', 'Male','Toronto','+19057819666')";
 
         transaction.executeSql(sql, [], function (tx, result) {
             alert("Insert success");
+
+//============================= CREATE CONTACTS CODE=================================
+            var myContact = navigator.contacts.create({"displayName": "The New Contact"});
+            var name = new ContactName();
+            name.givenName = "Jane";
+            name.familyName = "Doe";
+            myContact.name = name;
+
+            var phoneNumbers = [];
+            phoneNumbers[0] = new ContactField('work', '212-555-1234', false);
+            phoneNumbers[1] = new ContactField('mobile', '917-555-5432', true); // preferred number
+            phoneNumbers[2] = new ContactField('home', '203-555-7890', false);
+            myContact.phoneNumbers = phoneNumbers;
+
+            myContact.note = "Example note for the newly added contact";
+
+            myContact.save(onSuccessCallBack, onErrorCallBack);
+
+            function onSuccessCallBack(contact) {
+                alert("Save Success");
+            }
+            ;
+
+            function onErrorCallBack(contactError) {
+                alert("Error = " + contactError.code);
+            }
+            ;
+
             //showAllPressed()
         }, function (error) {
             alert("Insert failed: " + error);
